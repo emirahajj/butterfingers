11d37f49ace67cf9e9410f6203621d9d8d215bb1
Emira Hajj <emirahajj@gmail.com>
2021-01-28 21:46:52 -0500

    added quote.js

---
+++ src/quote.js
@@ -0,0 +1,8 @@
+async function randomQuote() {
+    const response = await fetch('https://api.quotable.io/random');
+    const data = await response.json();
+    return data.content;
+  }
+
+
+export default randomQuote;
